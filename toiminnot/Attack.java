f72d993f1f762d1bd1b0b1cb08cde2818b511038
ryunde <ryunde.taba@gmail.com>
2016-03-03 12:53:22 +0200

    häntä ja sakset

--- toiminnot/Attack.java
+++ toiminnot/Attack.java
@@ -6,7 +6,7 @@ public class Attack {
 	
 	public static void saksetin() {
 		
-		Motor.C.rotate(140); //sakset sis√§√§n
+		Motor.C.rotate(140); //sakset sis‰‰n
 	}
 	
 	public static void saksetout() {
@@ -16,7 +16,7 @@ public class Attack {
 	
 	public static void hanta() {
 		
-		Motor.D.rotate(15); // h√§nt√§ eteen
+		Motor.D.rotate(15); // h‰nt‰ eteen
 		Motor.D.rotate(-15); // h‰nt‰ taakse
 	}
 
