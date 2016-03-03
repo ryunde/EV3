53773ba8d25bd51641c1fffd728d31781450c3eb
ryunde <ryunde.taba@gmail.com>
2016-03-03 13:05:42 +0200

    selitykset lisätty

--- Drive.java
+++ Drive.java
@@ -9,18 +9,17 @@ public class Drive {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		LCD.clear();
-		LCD.drawString("NUU NUU", 0, 5);
-		Beep.aani();
+		LCD.clear(); 
+		LCD.drawString("NUU NUU", 0, 5);//kirjottaa ruutuun NUU NUU
+		Beep.aani(); // pit‰‰ beep ‰‰nen
 
 		// Get an instance of the IR EV3 sensor
 		
 		
-		final EV3IRSensor infraredSensor = new EV3IRSensor(SensorPort.S1);
+		final EV3IRSensor infraredSensor = new EV3IRSensor(SensorPort.S1); // infrapuna EV3 sensori-instanssi
 
-		RemoteControl checkerThread = new RemoteControl(
-				infraredSensor);
-		checkerThread.start();
+		RemoteControl checkerThread = new RemoteControl(infraredSensor); // uusi ohjain
+		checkerThread.start(); //alkaa huomioimaan objekteja
 		
 		
 	}
