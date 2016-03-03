ae8136a6bf9acf8e9cba0e231716221429b12eea
ryunde <ryunde.taba@gmail.com>
2016-03-03 13:13:19 +0200

    selitykset lisätty

--- RemoteControl.java
+++ RemoteControl.java
@@ -7,7 +7,7 @@ import lejos.hardware.sensor.EV3IRSensor;
 public class RemoteControl extends Thread {
 
 	
-    private EV3IRSensor infraredSensor;
+    private EV3IRSensor infraredSensor; //infrapunasensori p‰‰ss‰
     
     public RemoteControl(final EV3IRSensor sensor){
         this.infraredSensor = sensor;
@@ -15,28 +15,28 @@ public class RemoteControl extends Thread {
 
     public void run() {
 
-    	while(Button.ENTER.isUp()){
-            final int remoteCommand = infraredSensor.getRemoteCommand(0);
+    	while(Button.ENTER.isUp()){ //sill‰ aikaa kun ENTER nappulaa ei ole painittu, seuraavat toiminnot ovat voimassa:
+            final int remoteCommand = infraredSensor.getRemoteCommand(0); //sensorin tunnistama ohjaimen painikkeen (jota on painettu) numero
             switch (remoteCommand){
-                case 1:
+                case 1: //painike 1 (vas.yl)
                 	Movement.eteenpain(); //eteenp‰in
                     break;
-                case 2:
+                case 2: //painike 2 (vas.al)
                 	Movement.taaksepain(); //taaksep‰in
                 	break;
-                case 3:
+                case 3: //painike 3 (oik.yl)
                 	Movement.oikealle(); //oikealle                	
                 	break;
-                case 4:
+                case 4: //painike 4 (oik.al)
                 	Movement.vasemmalle(); //vasemmalle                	
                 	break;
-                case 5:
+                case 5: //painikkeet 1+3 
                 	Attack.saksetin(); //sakset sis‰‰n
                 	break;
-                case 6:
+                case 6: //painikkeet 1+4
                 	Attack.saksetout();  //sakset ulos
                 	break;
-                case 8:
+                case 8: //painikkeet 2+4
                 	Attack.hanta(); //h‰nt‰
             		break;
                 default:
