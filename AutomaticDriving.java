545fbecd066e0a8c0e63c68fae5a19e6c5a0a42a
ryunde <ryunde.taba@gmail.com>
2016-03-03 14:26:43 +0200

    automaattinen ajaminen v1

--- AutomaticDriving.java
+++ AutomaticDriving.java
@@ -12,17 +12,14 @@ import lejos.utility.Delay;
 public class AutomaticDriving {
 	private static SensorModes sensor;
 	public static void main(String[] args) {
-	// TODO Auto-generated method stub
        	
 	LCD.clear();
         LCD.drawString("NUU NUU", 0, 5);
-        
-	// get a port instance
+
         Port port = LocalEV3.get().getPort("S1");
         
         sensor = new EV3IRSensor(port);
         
-        // get an instance of this sensor in measurement mode
         SampleProvider distance= sensor.getMode("Distance");
         
         while (Button.ENTER.isUp()) {
@@ -32,11 +29,10 @@ public class AutomaticDriving {
         	Motor.A.forward();
     		Motor.B.forward();
         	
-        	// stack a filter on the sensor that gives the running average of the last 5 samples
         	SampleProvider average = new MeanFilter(distance, 5);
-        	// initialize an array of floats for fetching samples
+
         	float[] sample = new float[average.sampleSize()];
-        	// fetch a sample
+
         	average.fetchSample(sample, 0);
         	
         	int dist = (int) sample[0];
@@ -45,13 +41,13 @@ public class AutomaticDriving {
         		Motor.A.backward();
         		Motor.B.backward();
             	            	            	            	
-            	// stack a filter on the sensor that gives the running average of the last 5 samples
+
             	average = new MeanFilter(distance, 5);
             	
-            	// initialize an array of floats for fetching samples
+
             	sample = new float[average.sampleSize()];
             	
-            	// fetch a sample
+
             	average.fetchSample(sample, 0);
             	            	            	
             	dist = (int) sample[0];
