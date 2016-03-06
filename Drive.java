2d390783682f2383f869a911ddc79434db52d08c
ryunde <ryunde.taba@gmail.com>
2016-03-07 00:45:16 +0200

    vanha2

--- Drive.java
+++ Drive.java
@@ -1,33 +1,15 @@
-/**
- * Robotti NuuNuu
- * Pääohjelmaluokka Drive, jossa main metodin myötä luokka voidaan suorittaa 
- * sovelluksena.
- * Suorittaessa robotin ruudulle ilmestyy tekstiä ja kuuluu ääni.
- * Infrapunasensorin ja ohjaimen määrittely, jotta kauko-ohjaimesta tulevat
- * kommennot voidaan lukea.
- * 
- * @author Tuhmat Teletapit
- */
-
-import toiminnot.Beep;
 import lejos.hardware.lcd.LCD;
 import lejos.hardware.port.SensorPort;
 import lejos.hardware.sensor.EV3IRSensor;
-
 public class Drive {
-
 	public static void main(String[] args) {
-
-		LCD.clear(); 
-		LCD.drawString("NUU NUU", 0, 5);//kirjottaa ruutuun NUU NUU
-		Beep.aani(); // pitää beep äänen
-		
-		final EV3IRSensor infraredSensor = new EV3IRSensor(SensorPort.S1); // infrapuna EV3 sensori-instanssi
-
-		RemoteControl checkerThread = new RemoteControl(infraredSensor); // uusi ohjain
-		checkerThread.start(); //alkaa huomioimaan objekteja
-		
-		
+		// TODO Auto-generated method stub
+		LCD.clear();
+		LCD.drawString("NUU NUU", 0, 0);
+		// Get an instance of the IR EV3 sensor
+		final EV3IRSensor infraredSensor = new EV3IRSensor(SensorPort.S1);
+		RemoteControl checkerThread = new RemoteControl(
+				infraredSensor);
+		checkerThread.start();
 	}
-
 }
--- RemoteControl.java
+++ RemoteControl.java
@@ -43,21 +43,3 @@ public class RemoteControl extends Thread {
     }    
     
 }
-Ryunde Taba
-2/5, 12:05pm
-Ryunde Taba
-import lejos.hardware.lcd.LCD;
-import lejos.hardware.port.SensorPort;
-import lejos.hardware.sensor.EV3IRSensor;
-public class Drive {
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		LCD.clear();
-		LCD.drawString("NUU NUU", 0, 0);
-		// Get an instance of the IR EV3 sensor
-		final EV3IRSensor infraredSensor = new EV3IRSensor(SensorPort.S1);
-		RemoteControl checkerThread = new RemoteControl(
-				infraredSensor);
-		checkerThread.start();
-	}
-}
