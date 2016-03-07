01a9fbc155de309eb7b1ea4b6b57423a4d8514d3
ryunde <ryunde.taba@gmail.com>
2016-03-07 17:23:13 +0200

    värin lisäys, toinen ääni ja refaktorointi

--- toiminnot/Attack.java
+++ toiminnot/Attack.java
@@ -1,30 +1,38 @@
 /**
- * H‰nn‰n ja Saksien luokka. H‰nt‰ liikkuu eteenp‰in ja taaksep‰in. Sakset
- * sulkeutuvat ja aukeavat.
- * 
+ * H‰nn‰n ja Saksien sek‰ valon v‰l‰ytyksen luokka. H‰nt‰ liikkuu eteenp‰in ja
+ * taaksep‰in. Sakset sulkeutuvat ja aukeavat. Punainen vilkkuva valo ja valot pois.
+ *
  * @author Tuhmat Teletapit
  */
-
 package toiminnot;
 
 import lejos.hardware.motor.Motor;
+import lejos.hardware.Button;
 
 public class Attack {
-	
-	public static void saksetin() {
-		
-		Motor.C.rotate(140); //sakset sis‰‰n
-	}
-	
-	public static void saksetout() {
-		
-		Motor.C.rotate(-140); //sakset ulos
-	}
-	
-	public static void hanta() {
-		
-		Motor.D.rotate(15); // h‰nt‰ eteen
-		Motor.D.rotate(-15); // h‰nt‰ taakse
-	}
+
+    public static void saksetin() {
+
+        Motor.C.rotate(140); //sakset sis‰‰n
+    }
+
+    public static void saksetout() {
+
+        Motor.C.rotate(-140); //sakset ulos
+    }
+
+    public static void hanta() {
+
+        Motor.D.rotate(15); // h‰nt‰ eteen
+        Motor.D.rotate(-15); // h‰nt‰ taakse
+    }
+
+    public static void punainen() {
+        Button.LEDPattern(8);
+    }
+
+    public static void valopois() {
+        Button.LEDPattern(0);
+    }
 
 }
--- toiminnot/Beep.java
+++ toiminnot/Beep.java
@@ -1,23 +1,30 @@
 /**
- * Biippaus-luokka. Soittaa ‰‰nitiedoston ja etuk‰teen EV3-bricksin sis‰lt‰m‰n 
- * biippausj‰rjestyksen.
- * 
+ * Biippaus-luokka. Metodit soittavat ‰‰nitiedoston ja etuk‰teen EV3-bricksin 
+ * sis‰lt‰m‰n biippausj‰rjestyksen.
+ *
  * @author Tuhmat Teletapit
  */
-
 package toiminnot;
+
 import java.io.File;
 import lejos.hardware.Sound;
 
 public class Beep {
-	
-public static void aani(){
-	
-	File music = new File("hanta2.wav"); //‰‰nitiedosto
-	Sound.playSample(music, 100); //soita tiedosto
-	Sound.setVolume(100); // ‰‰nen voimakkuus
-	Sound.beepSequence(); //varmuuden vuoksi jos ‰‰nitiedosto ei jostain syyst‰ toimi
-	
-	}
+
+    public static void aani() {
+
+        File music = new File("hanta2.wav"); //‰‰nitiedosto
+        Sound.playSample(music, 100); //soita tiedosto
+        Sound.setVolume(100); // ‰‰nen voimakkuus
+        Sound.beepSequence(); //varmuuden vuoksi jos ‰‰nitiedosto ei jostain syyst‰ toimi
+
+    }
+
+    public static void aani2() {
+        File music = new File("hanta1.wav"); //‰‰nitiedosto
+        Sound.playSample(music, 100); //soita tiedosto
+        Sound.setVolume(100); // ‰‰nen voimakkuus
+        Sound.beepSequence(); //varmuuden vuoksi jos ‰‰nitiedosto ei jostain syyst‰ toimi
+    }
 
 }
