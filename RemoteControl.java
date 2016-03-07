429db9bd13e6882247a0210e16d896ff75ae0047
tundet <ryunde.taba@gmail.com>
2016-03-07 21:04:22 +0200

    valojen doc

--- RemoteControl.java
+++ RemoteControl.java
@@ -1,9 +1,10 @@
 
 /**
  * Kauko-ohjaimen luokka, jossa sijaitsee metodi kauko-ohjaimen painettujen
- * nappuloiden huomioiminen ja niihin reagoiminen tietyll‰ tavalla. Erilaisia
- * toimintoja on 7; eteenp‰in, taaksep‰in, oikealle ja vasemmalle k‰‰ntyminen
- * sek‰ h‰nn‰n ja saksien liikuttaminen.
+ * nappuloiden huomioiminen ja niihin reagoiminen tietyll√§ tavalla. Aluksi n√§ytt√∂√∂n
+ * ilmestyy teksti ja kuuluu beep-√§√§ni. Erilaisia toimintoja on 7; eteenp√§in, taaksep√§in, 
+ * oikealle ja vasemmalle k√§√§ntyminen sek√§ h√§nn√§n ja saksien liikuttaminen. H√§nn√§n ja saksien
+ * liikkumisen yhteydess√§ vilkkuu my√∂s punainen valo.
  *
  * @author Tuhmat Teletapit
  */
@@ -16,7 +17,7 @@ import lejos.hardware.lcd.LCD;
 
 public class RemoteControl extends Thread {
 
-    private EV3IRSensor infraredSensor; //infrapunasensori p‰‰ss‰
+    private EV3IRSensor infraredSensor; //infrapunasensori p√§√§ss√§
 
     public RemoteControl(final EV3IRSensor sensor) {
         this.infraredSensor = sensor;
@@ -26,18 +27,18 @@ public class RemoteControl extends Thread {
 
         LCD.clear();
         LCD.drawString("NUU NUU", 0, 5);//kirjottaa ruutuun NUU NUU
-        Beep.aani(); // pit‰‰ beep ‰‰nen
+        Beep.aani(); // pit√§√§ beep √§√§nen
 
-        while (Button.ENTER.isUp()) { //sill‰ aikaa kun ENTER nappulaa ei ole painittu, seuraavat toiminnot ovat voimassa:7
+        while (Button.ENTER.isUp()) { //sill√§ aikaa kun ENTER nappulaa ei ole painittu, seuraavat toiminnot ovat voimassa:
 
             final int remoteCommand = infraredSensor.getRemoteCommand(0); //sensorin tunnistama ohjaimen painikkeen (jota on painettu) numero
 
             switch (remoteCommand) {
                 case 1: //painike 1 (vas.yl)
-                    Movement.eteenpain(); //eteenp‰in
+                    Movement.eteenpain(); //eteenp√§in
                     break;
                 case 2: //painike 2 (vas.al)
-                    Movement.taaksepain(); //taaksep‰in
+                    Movement.taaksepain(); //taaksep√§in
                     break;
                 case 3: //painike 3 (oik.yl)
                     Movement.oikealle(); //oikealle                	
@@ -46,20 +47,20 @@ public class RemoteControl extends Thread {
                     Movement.vasemmalle(); //vasemmalle                	
                     break;
                 case 5: //painikkeet 1+3 
-                    Attack.saksetin(); //sakset sis‰‰n
-                    Attack.punainen(); //punainen v‰lkkyv‰ valo
+                    Attack.saksetin(); //sakset sis√§√§n
+                    Attack.punainen(); //punainen v√§lkkyv√§ valo
                     break;
                 case 6: //painikkeet 1+4
                     Attack.saksetout();  //sakset ulos
-                    Attack.punainen(); //punainen v‰lkkyv‰ valo
+                    Attack.punainen(); //punainen v√§lkkyv√§ valo
                     break;
                 case 8: //painikkeet 2+4
-                    Attack.hanta(); //h‰nt‰
-                    Attack.punainen(); //punainen v‰lkkyv‰ valo
+                    Attack.hanta(); //h√§nt√§
+                    Attack.punainen(); //punainen v√§lkkyv√§ valo
                     break;
                 default:
-                    Movement.pysahtyy(); //pys‰htynyt
-                    Attack.valopois(); //ei v‰lkkyvi‰ valoja
+                    Movement.pysahtyy(); //pys√§htynyt
+                    Attack.valopois(); //ei v√§lkkyvi√§ valoja
 
             }
 
